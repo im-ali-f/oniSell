7ca195ad5dfa07260c1bf68c3badd5761e2a9c0b
im-ali-f <alifarhad.asus@gmail.com>
2023-03-15 00:28:04 +0330

    html basics complete + add css file

--- homePage.php
+++ homePage.php
@@ -5,14 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Oni Sell</title>
+    <link rel="stylesheet" href="homePage.css">
 </head>
 <body>
     <div class="wholePage">
         <div class="logo"></div>
-        <div class="menues"></div>
-        <div class="homeText"></div>
-        <div class="search"></div>
-        <div class="mostViewedCitys"></div>
+        <div class="menues">
+            <div class="addAdv">آگهی جدید</div>
+            <div class="aboutUs">درباره ما</div>
+            <div class="support">پشتیبانی</div>
+        </div>
+        <div class="homeText">
+            <p class="homeTextTxt">آنی سل ﭘﺎﯾﮕﺎه ﺧﺮﯾﺪ و ﻓﺮوش ﺑﯽ‌واﺳﻄﻪ‌
+                اﮔﻪ دﻧﺒﺎل ﭼﯿﺰی ﻫﺴﺘﯽ، ﺷﻬﺮت رو اﻧﺘﺨﺎب ﮐﻦ و ﺗﻮ دﺳﺘﻪ‌ﺑﻨﺪی‌ﻫﺎ ﺑﻪ دﻧﺒﺎﻟﺶ ﺑﮕﺮد. اﮔﺮ ﻫﻢ ﻣﯽ‌ﺧﻮای ﭼﯿﺰی ﺑﻔﺮوﺷﯽ، ﭼﻨﺪ ﺗﺎ ﻋﮑﺲ ﺧﻮب ازش ﺑﮕﯿﺮ و آگهی بزار.
+            </p>
+        </div>
+        <div class="search">
+            <form action="homePage.php" method="post">
+                <input type="search" name="" id="">
+            </form>
+        </div>
+        <div class="mostViewedCitysSection">
+            <p class="mostViewedCitysTxt">شهر های پر بازدید</p>
+            <div class="mostViewedcitys"></div>
+        </div>
         <div class="socialMedias"></div>
     </div>
 </body>
