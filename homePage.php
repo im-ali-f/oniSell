db0f4ecb102a7f9127914d7e0bd534712846be38
im-ali-f <alifarhad.asus@gmail.com>
2023-03-14 23:58:50 +0330

    files created lets start!

---
+++ homePage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oni Sell</title>
+</head>
+<body>
+    <div class="wholePage">
+        <div class="logo"></div>
+        <div class="menues"></div>
+        <div class="homeText"></div>
+        <div class="search"></div>
+        <div class="mostViewedCitys"></div>
+        <div class="socialMedias"></div>
+    </div>
+</body>
+</html>
