c3936d264a5fef8fdc8d194cf93065945420572c
im-ali-f <alifarhad.asus@gmail.com>
2023-03-18 22:06:35 +0330

    logo added and some changes

--- homePage.php
+++ homePage.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,20 +8,23 @@
     <title>Oni Sell</title>
     <link rel="stylesheet" href="homePage.css">
 </head>
+
 <body>
     <div class="wholePage">
-        <div class="logo">logo</div>
+        <div class="logo">
+            <img src="/assets/images/logonomy-1679158691376.svg" alt="OniSellLogo">
+        </div>
         <div class="menus homePageMargin">
             <a href="#" class="addAdv">آگهی جدید</a>
             <a href="#" class="aboutUs">درباره ما</a>
             <a href="#" class="support">پشتیبانی</a>
-            
+
         </div>
         <div class="separator"></div>
 
         <div class="homeText homePageMargin">
             <p class="homeTextTxt">آنی سل ﭘﺎﯾﮕﺎه ﺧﺮﯾﺪ و ﻓﺮوش ﺑﯽ‌واﺳﻄﻪ‌
-               <br> اﮔﻪ دﻧﺒﺎل ﭼﯿﺰی ﻫﺴﺘﯽ، ﺷﻬﺮت رو اﻧﺘﺨﺎب ﮐﻦ و ﺗﻮ دﺳﺘﻪ‌ﺑﻨﺪی‌ﻫﺎ ﺑﻪ دﻧﺒﺎﻟﺶ ﺑﮕﺮد. اﮔﺮ ﻫﻢ ﻣﯽ‌ﺧﻮای ﭼﯿﺰی ﺑﻔﺮوﺷﯽ، ﭼﻨﺪ ﺗﺎ ﻋﮑﺲ ﺧﻮب ازش ﺑﮕﯿﺮ و آگهی بزار
+                <br> اﮔﻪ دﻧﺒﺎل ﭼﯿﺰی ﻫﺴﺘﯽ، ﺷﻬﺮت رو اﻧﺘﺨﺎب ﮐﻦ و ﺗﻮ دﺳﺘﻪ‌ﺑﻨﺪی‌ﻫﺎ ﺑﻪ دﻧﺒﺎﻟﺶ ﺑﮕﺮد. اﮔﺮ ﻫﻢ ﻣﯽ‌ﺧﻮای ﭼﯿﺰی ﺑﻔﺮوﺷﯽ، ﭼﻨﺪ ﺗﺎ ﻋﮑﺲ ﺧﻮب ازش ﺑﮕﯿﺮ و آگهی بزار
         </div>
         <div class="searchSection homePageMargin">
             <form action="homePage.php" method="post">
@@ -32,8 +36,9 @@
             <div class="mostViewedcitys"></div>
         </div>
         <div class="separator homePageMargin"></div>
-        
+
         <div class="socialMedias homePageMargin">ffff</div>
     </div>
 </body>
+
 </html>
