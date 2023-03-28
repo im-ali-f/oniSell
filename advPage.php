4fca21c706416a2a012a162c01fb8c45a788739a
Ali Farhad <115785932+im-ali-f@users.noreply.github.com>
2023-03-28 21:43:43 +0430

    complete creating html sections

--- advPage.php
+++ advPage.php
@@ -5,8 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ADVERTISEMENT Page</title>
+    <link rel="stylesheet" href="advPage.css">
 </head>
 <body>
-    
+    <div class="wholePage">
+        <div class="menuBar">
+        
+            <div class="menuContainer">
+                <a href="#"class="newAdvertisement menuA">آگهی جدید</a>
+                <a href="#"class="support menuA">پشتیبانی</a>
+                <a href="#"class="profile menuA">پروفایل</a>
+            </div>
+            <div class="searchAdv">ddd</div>
+
+            <div link="#" class="logo">
+            <img src="/assets/images/logonomy-1679158691376.svg" alt="OniSellLogo">
+            </div> 
+
+        </div>
+        
+        <div class="advBody">
+            <div class="advSection"></div>
+            <div class="optionsSection">
+
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
