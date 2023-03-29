e9f22c3c393919d8b5ce18e35721a95d4a7ff52d
Ali Farhad <115785932+im-ali-f@users.noreply.github.com>
2023-03-29 19:17:05 +0430

    creating options section

--- advPage.php
+++ advPage.php
@@ -26,58 +26,6 @@
         
         <div class="advBody">
             <div class="advSection">
-            <div class="adv">
-                    <div class="advImage">
-                        <img class="img" src="/oniSell/assets/test.jpg" alt="" srcset="">
-                    </div>
-                    <div class="advTexts">
-                        <div class="title">تابلو تست عالی ب بله ودیگه </div>
-                        <div class="description">
-                            <div class="condition">نو</div>
-                            <div class="price">91  تومان</div>
-                            <div class="addDate">i d k</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="adv">
-                    <div class="advImage">
-                        <img class="img" src="/oniSell/assets/test.jpg" alt="" srcset="">
-                    </div>
-                    <div class="advTexts">
-                        <div class="title">تابلو تست عالی ب بله ودیگه </div>
-                        <div class="description">
-                            <div class="condition">نو</div>
-                            <div class="price">91  تومان</div>
-                            <div class="addDate">i d k</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="adv">
-                    <div class="advImage">
-                        <img class="img" src="/oniSell/assets/test.jpg" alt="" srcset="">
-                    </div>
-                    <div class="advTexts">
-                        <div class="title">تابلو تست عالی ب بله ودیگه </div>
-                        <div class="description">
-                            <div class="condition">نو</div>
-                            <div class="price">91  تومان</div>
-                            <div class="addDate">i d k</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="adv">
-                    <div class="advImage">
-                        <img class="img" src="/oniSell/assets/test.jpg" alt="" srcset="">
-                    </div>
-                    <div class="advTexts">
-                        <div class="title">تابلو تست عالی ب بله ودیگه </div>
-                        <div class="description">
-                            <div class="condition">نو</div>
-                            <div class="price">91  تومان</div>
-                            <div class="addDate">i d k</div>
-                        </div>
-                    </div>
-                </div>
                 <div class="adv">
                     <div class="advImage">
                         <img class="img" src="/oniSell/assets/test.jpg" alt="" srcset="">
@@ -94,10 +42,40 @@
                 
             </div>
             <div class="optionsSection">
-
+                <div class="priceOption">
+                    (به تومان) قیمت حداقل<input type="number" name="minPrice" id="minPrice">
+                    (به تومان) قیمت حداکثر<input type="number" name="maxPrice" id="maxPrice">
+                </div>
+                <div class="seperator"></div>
+               <select id="selectCondition" name="selectCondition" class="select">
+                    <option value="">وضعیت محصول</option>
+                    <option value="نو">نو</option>
+                    <option value="در حد نو">در حد نو</option>
+                    <option value="کارکرده">کارکرده</option>
+                    <option value="کهنه">کهنه</option>
+                </select>
+                <div class="sepertor"></div>
+                دسته بندی<div class="group formElement divSelect">
+                            <select id="selectGroup" name="selectGroup" class="select" required>
+                                <option value="">یکی از شاخه ها را انتخاب کنید</option>
+                                <option value="realestate">املاک</option>
+                                <option value="vehicle">وسیله نقلیه</option>
+                                <option value="digital">لوازم دیجیتال</option>
+                                <option value="house">خانه و اشپزخانه</option>
+                                <option value="service">سرویس و خدمات</option>
+                                <option value="personal">وسایل شخصی</option>
+                                <option value="entertainment">سرگرمی و فراغت</option>
+                                <option value="social">اجتماعی</option>
+                                <option value="equipment">تجهیزات صنعتی</option>
+                            </select>
+                        </div>
+            زیر شاخه<div class="subGroup formElement divSelect">
+                <select id="selectSubGroup" name="selectSubGroup" class="select" required disabled>
+                </select>
+            </div>
             </div>
         </div>
     </div>
 </body>
-
+<script src="advPage.js"></script>
 </html>
