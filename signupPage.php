7025c4277eb5597c7d22a84ad39196dd109e4ccc
Ali Farhad <115785932+im-ali-f@users.noreply.github.com>
2023-04-03 17:19:31 +0430

    signup check password fixed + add adv html + csss files

--- signupPage.php
+++ signupPage.php
@@ -21,13 +21,14 @@
                     <?php
                     if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         try {
-                            $db = new PDO("mysql:host=www.onisell.ir;dbname=fijmaclt_accounts", "fijmaclt", "bd83Y7t3rF");
+                            $db = new PDO("mysql:host=www.onisell.ir;dbname=fijmaclt_onisellDataBase", "fijmaclt", "bd83Y7t3rF");
                         } catch (PDOException $e) {
                             die("ye error !" . $e->getmessage());
                         }
                         $name = $_POST["userName"];
                         $email = $_POST["email"];
                         $pass = $_POST["password1"];
+                        $pass2 = $_POST["password2"];
 
                         $queryName = "SELECT * FROM `acc` WHERE `name` LIKE \"$name\"";
                         $queryEmail = "SELECT * FROM `acc` WHERE `email` LIKE \"$email\"";
@@ -38,21 +39,25 @@
                         $resultEmail = $db->query($queryEmail);
                         $rowsEmail = $resultEmail->fetch();
 
-
-                        if (!$rowsName) {
-                            if (!$rowsEmail) {
-                                $queryCreateNewAcc = "
+                        if ($pass == $pass2) {
+                            if (!$rowsName) {
+                                if (!$rowsEmail) {
+                                    $queryCreateNewAcc = "
                             INSERT INTO acc (name, pass, email)
                             VALUES (\"$name\",\"$pass\",\"$email\");";
-                                $db->exec($queryCreateNewAcc);
-                                echo "<div class=\"error\"> ثبت نام کامل شد لطفا به حساب خود وارد شوید</div>";
-                                echo "<a href=\"loginPage.php\"><button class=\"BTN redirectBTN\" id=\"redirectToLogin\">صفحه ورود</button></a>";
-                                exit();
+                                    $db->exec($queryCreateNewAcc);
+                                    echo "<div class=\"error\"> ثبت نام کامل شد لطفا به حساب خود وارد شوید</div>";
+                                    echo "<a href=\"loginPage.php\"><button class=\"BTN redirectBTN\" id=\"redirectToLogin\">صفحه ورود</button></a>";
+                                    exit();
+                                } else {
+                                    echo "<div class=\"error\"> کاربری با همین ایمیل قبلا ثبت نام کرده است</div>";
+                                }
                             } else {
-                                echo "<div class=\"error\"> کاربری با همین ایمیل قبلا ثبت نام کرده است</div>";
+                                echo "<div class=\"error\"> کاربری با همین نام قبلا ثبت نام کرده است</div>";
                             }
-                        } else {
-                            echo "<div class=\"error\"> کاربری با همین نام قبلا ثبت نام کرده است</div>";
+                        } 
+                        else {
+                            echo "<div class=\"error\">پسورد و تکرار پسوورد یکسان نیست  </div>";
                         }
                     }
 
@@ -62,11 +67,11 @@
 
                 </div>
                 <p class="signupText">اطلاعات را برای ورود وارد کنید</p>
-                <form class="signupForm" action="signupPage.php" method="post" require>
-                    Username<input type="text" class="textInput signupPageMargin" name="userName" id="userName">
-                    Email<input type="email" class="textInput signupPageMargin" name="email" id="email">
-                    Password <input type="password" class="textInput signupPageMargin" name="password1" id="password1">
-                    reenter Password <input type="password" class="textInput signupPageMargin" name="password2" id="password2">
+                <form class="signupForm" action="signupPage.php" method="post" required>
+                    Username<input type="text" class="textInput signupPageMargin" name="userName" id="userName" required>
+                    Email<input type="email" class="textInput signupPageMargin" name="email" id="email" required>
+                    Password <input type="password" class="textInput signupPageMargin" name="password1" id="password1" required>
+                    reenter Password <input type="password" class="textInput signupPageMargin" name="password2" id="password2" required>
                     <button class="BTN" type="submit"> ثبت نام</button>
                 </form>
             </div>
