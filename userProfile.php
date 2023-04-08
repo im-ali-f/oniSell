a6b8dc561b37f79cb2c318e6eeff692cdebe793e
Ali Farhad <115785932+im-ali-f@users.noreply.github.com>
2023-04-08 23:50:00 +0430

    add menu for userProfile Page

--- userProfile.php
+++ userProfile.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="userProfile.css">
 </head>
+
 <body>
-    
+    <div class="wholePage">
+
+        <div class="menuBar">
+
+            <div class="menuContainer">
+                <a href="#" class="newAdvertisement menuA">آگهی جدید</a>
+                <a href="#" class="support menuA">پشتیبانی</a>
+            </div>
+            <div link="#" class="logo">
+                <img src="/assets/images/logonomy-1679158691376.svg" alt="OniSellLogo">
+            </div>
+
+        </div>
+
+    </div>
 </body>
+
 </html>
